46cdd1ea8f2a33624ca534a7ea30c8faca4256d7
hiep1998vnhn11 <hiep1998vnhn11@gmail.com>
2020-12-14 22:10:29 +0700

    new message buttom

--- src/pages/Main/Main.vue
+++ src/pages/Main/Main.vue
@@ -4,18 +4,30 @@
     <v-container>
       <router-view :loading="loading" />
     </v-container>
+    <new-message />
+    <v-btn class="button-navigation white elevation-20" x-large icon text>
+      <v-icon color="black" v-text="messageIcon"></v-icon>
+    </v-btn>
   </v-main>
 </template>
 
 <script>
 import AppBar from '@/components/Layout/AppBar'
 import { mapActions, mapGetters } from 'vuex'
+import NewMessage from '@/components/Message/NewMessage'
+
 export default {
   components: {
-    'app-bar': AppBar
+    'app-bar': AppBar,
+    NewMessage
   },
   computed: {
-    ...mapGetters('user', ['currentUser'])
+    ...mapGetters('user', ['currentUser']),
+    messageIcon() {
+      return this.$route.name === 'Home'
+        ? 'mdi-pencil-circle-outline'
+        : 'mdi-account-multiple-outline'
+    }
   },
   data() {
     return {
@@ -46,5 +58,12 @@ export default {
 .card-dialog {
   position: absolute;
   z-index: 100;
+}
+
+.button-navigation {
+  position: fixed;
+  z-index: 9999;
+  bottom: 15px;
+  right: 15px;
 }
 </style>
